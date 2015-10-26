59f71f25607da77f7b47f438393b5068838bcc20
cavb1205 <cavb.1205@hotmail.com>
2015-10-26 17:48:10 -0500

    se agregaron los estilos en el nav bar y los estilos en tablas para mostrar usuarios, equipos prestamos

--- audiovisuales/settings.py
+++ audiovisuales/settings.py
@@ -103,4 +103,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR,'audiovisuales/media'),
+)
